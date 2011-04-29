37eaf501823b8d4f7f6aa1becf8c7871a48f43a4
combiochem <combiochem@yahoo.com>
2011-04-29 15:15:01 -0700

    Edited endoClip/script.getALLSNR.sh via GitHub

--- endoClip/script.getALLSNR.sh
+++ endoClip/script.getALLSNR.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 for i in 50 60; do for k in {0..3}; do for j in norepeat repeat; do python script.getSNRStats.py runK50GU/oRNA.data.1.1.$i.$k.$j >> ALL.SNR; done; done; done
+
+jkjkjkkjkj
